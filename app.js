b1e470da3f37e73eee1e4fbfc4e2c1d922878821
Samiksha Sapkota <sapkota.samiksha@gmail.com>
2023-10-03 19:31:22 -0500

    update

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const cors = require("cors");
 const userRoutes = require("./src/modules/user/userRoutes");
 const dataRoutes = require("./src/modules/data/dataRoutes");
 
+// Define the origin of your React app (replace with your actual URL)
+const allowedOrigins = ["https://weather-app-using-react-express.vercel.app"];
+
+// Set up CORS options
 const corsOptions = {
-  origin: "*", // Replace with the actual URL of your React app
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11) may require 200 status
 };
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
