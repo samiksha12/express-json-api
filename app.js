4798de028570b5f61406c765c308054075ccfc20
Samiksha Sapkota <sapkota.samiksha@gmail.com>
2023-10-03 19:33:40 -0500

    update

--- app.js
+++ app.js
@@ -22,6 +22,19 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://weather-app-using-react-express.vercel.app'); // Replace with your React app's URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Respond to preflight requests
+  } else {
+    next();
+  }
+});
+
 app.use("/users", userRoutes);
 app.use("/data", dataRoutes);
 
